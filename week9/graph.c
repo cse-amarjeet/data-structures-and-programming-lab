4ec080c4315f057573c1c03b86ddb5e02c481de9
ajroni98 <93990485+ajroni98@users.noreply.github.com>
2022-01-12 15:06:50 +0530

    Update graph.c

--- week9/graph.c
+++ week9/graph.c
@@ -186,33 +186,3 @@ void main()
 		
 	}
 }
-
-/*
-int main(){
-    struct queue q;
-    q.size = 4;
-    q.f = q.r = 0;
-    q.arr = (int*) malloc(q.size*sizeof(int));
-    
-    // Enqueue few elements
-    enqueue(&q, 12);
-    enqueue(&q, 15);
-    enqueue(&q, 1); 
-    printf("Dequeuing element %d\n", dequeue(&q));
-    printf("Dequeuing element %d\n", dequeue(&q));
-    printf("Dequeuing element %d\n", dequeue(&q)); 
-    enqueue(&q, 45);
-    enqueue(&q, 45);
-    enqueue(&q, 45);
- 
-    if(isEmpty(&q)){
-        printf("Queue is empty\n");
-    }
-    if(isFull(&q)){
-        printf("Queue is full\n");
-    }
- 
-    return 0;
-}
-
-*/
